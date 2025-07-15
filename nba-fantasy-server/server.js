41608ecd1644a3d342e142b8f8fe24c5cbd41d90
Jackie Lin <JLin226859643@gmail.com>
2025-07-14 21:53:05 -0400

    back on render

--- nba-fantasy-server/server.js
+++ nba-fantasy-server/server.js
@@ -7,10 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: true, // Allow all origins for now
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // In-memory cache for getNBATeams data
